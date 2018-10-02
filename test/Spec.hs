325df31aa2a4bee710ae9b803ddd5535fb3a850f
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-10-02 12:36:54 +0200

    works
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works
    
    works

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,40 @@
 import Test.Hspec
 
+import Data.Maybe
+import Data.Char
+import Data.List as L
+
+greet :: Maybe String -> String
+greet Nothing = "Hello, my friend"
+greet (Just name) = if L.all isUpper name  
+  then "HELLO " ++ name ++ "!"
+  else "Hello " ++ name
+
+greet2 :: Either [String] (Maybe String) -> String
+greet2 (Right m) = greet m
+greet2 (Left (n:m:ns)) = "Hello, " ++ allButLast ++ " and " ++ lastName where
+  allButLast = "Jill"
+  lastName = "Jane"
+greet2 (Left (n:ns)) = "Hello, " ++ n
+greet2 (Left ns) = "Hello, " ++ (intercalate " and " ns)
+
 main :: IO ()
 main = hspec $
   describe "greeting" $ do
-    it "does ..." $
-        1 `shouldBe` 2
+    it "greeting Bob should display Hello Bob" $
+      greet2 (Right (Just "Bob")) `shouldBe` "Hello Bob"
+
+    it "if name is null we should display Hello, my friend" $
+      greet Nothing `shouldBe` "Hello, my friend"
+
+    it "if name is uppercase we should display greeting in uppercase" $ 
+      greet (Just "JERRY") `shouldBe` "HELLO JERRY!"
+    it "if name is uppercase we should display greeting in uppercase" $ 
+      greet (Just "BOB") `shouldBe` "HELLO BOB!"
+  
+    it "greets everyone in a list" $ 
+      greet2 (Left ["John"]) `shouldBe` "Hello, John"
+    it "greets everyone in a list" $ 
+      greet2 (Left ["Jill"]) `shouldBe` "Hello, Jill"
+    it "greets everyone in a list" $ 
+      greet2 (Left ["Jill",  "Jane"]) `shouldBe` "Hello, Jill and Jane"
