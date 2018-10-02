8fbeb2d6f4046ddd2559d95bba68772e658a666f
Remy Sanlaville <remy.sanlaville@gmail.com>
2018-10-02 12:48:50 +0200

    Manage when name is null

--- src/main/java/org/dojo/kata/Greeting.java
+++ src/main/java/org/dojo/kata/Greeting.java
@@ -3,6 +3,8 @@ package org.dojo.kata;
 public class Greeting {
 
 	public String greet(String name) {
+		if (name == null)
+			return "Hello, my friend.";
 		return "Hello, " + name + ".";
 	}
 
--- src/test/java/org/dojo/kata/GreetingTest.java
+++ src/test/java/org/dojo/kata/GreetingTest.java
@@ -17,4 +17,17 @@ public class GreetingTest {
 		// Then
 		Assertions.assertThat(greetingName).isEqualTo("Hello, Bob.");
 	}
+
+	@Test
+	public void greeting_with_null_returns_Hello_my_friend() {
+		// Given
+		String name = null;
+		Greeting gretting = new Greeting();
+
+		// When
+		String greetingName = gretting.greet(name);
+
+		// Then
+		Assertions.assertThat(greetingName).isEqualTo("Hello, my friend.");
+	}
 }
